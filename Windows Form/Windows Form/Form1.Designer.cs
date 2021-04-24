d778e46f241bbf498a5a483381716c260216018a
anzh <47479350+anzh73@users.noreply.github.com>
2021-04-24 06:45:23 +0300

    Add files via upload

---
+++ Windows Form/Windows Form/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Windows_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxF1 = new System.Windows.Forms.TextBox();
+            this.buttonF1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxF1
+            // 
+            this.textBoxF1.Location = new System.Drawing.Point(40, 32);
+            this.textBoxF1.Name = "textBoxF1";
+            this.textBoxF1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxF1.TabIndex = 0;
+            // 
+            // buttonF1
+            // 
+            this.buttonF1.Location = new System.Drawing.Point(51, 79);
+            this.buttonF1.Name = "buttonF1";
+            this.buttonF1.Size = new System.Drawing.Size(75, 23);
+            this.buttonF1.TabIndex = 1;
+            this.buttonF1.Text = "button1";
+            this.buttonF1.UseVisualStyleBackColor = true;
+            this.buttonF1.Click += new System.EventHandler(this.buttonF1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonF1);
+            this.Controls.Add(this.textBoxF1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxF1;
+        private System.Windows.Forms.Button buttonF1;
+    }
+}
+
---
+++ Windows Form/Windows Form/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public string SolData
+        {
+            get
+            { return textBoxF1.Text; }
+            set
+            { textBoxF1.Text = value; }
+        }
+
+        private void buttonF1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonF1_Click_1(object sender, EventArgs e)
+        {
+            Form2 frm2 = new Form2();
+            frm2.Show(this);
+        }
+    }
+}
