56ef685f2567b35f0c3c6cc8a445bce0f7c61594
anzh <47479350+anzh73@users.noreply.github.com>
2021-04-24 06:38:18 +0300

    Add files via upload

---
+++ DataAdapterWizard/DataAdapterWizard/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataAdapterWizard
+{
+    public partial class Form1 : Form
+    {
+        private object northwindDataSet1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = northwindDataSet1.Customers;            sqlDataAdapter1.Fill(northwindDataSet1.Customers);
+        }
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            sqlDataAdapter1.Update(northwindDataSet1);
+        }
+    }
+}
