18a8d9649ab41b7f81084b78901c6f68003f7c37
anzh <47479350+anzh73@users.noreply.github.com>
2021-04-24 06:47:37 +0300

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/nForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class nForm : WindowsFormsApp2.Form1
+    {
+        public nForm()
+        {
+            InitializeComponent();
+        }
+
+        private void nForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public nForm(IContainer components)
+        {
+            this.components = components;
+        }
+
+        void nForm.Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new nForm());
+        }
+    }
+}
